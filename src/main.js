5c8c56a34f515aeeb8108455f90abf8975a04c34
Prasert Saengkaew <kengsert@gmail.com>
2024-11-24 23:28:21 +0700

    fix filter feature and fix search feature

--- src/main.js
+++ src/main.js
@@ -5,16 +5,14 @@ let filteredPokemonData = []
 document.getElementById('prev-btn').addEventListener('click', () => changePage(-1))
 document.getElementById('next-btn').addEventListener('click', () => changePage(1))
 
-fetchPokemon()
+async function initializePage() {
+    await fetchPokemon()
+    renderPokemonPage()
+}
 
-// async function initializePage() {
-//     await fetchPokemon(1)
-//     renderPokemon()
-// }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     initializePage()
-// })
+document.addEventListener('DOMContentLoaded', () => {
+    initializePage()
+})
 
 async function fetchPokemon() {
     const loader = document.querySelector('.js-loader')
@@ -23,26 +21,24 @@ async function fetchPokemon() {
     grid.classList.add('hidden')
 
     try {
-        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=1200`)
+        const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=200`)
         if (!response.ok) throw new Error('Failed to fetch Pokémon list')
         const data = await response.json()
-        //console.log(data)
-        // const promises = data.results.map((pokemon) => fetch(pokemon.url).then((res) => res.json()))
+
+        // ดึงข้อมูลเพิ่มเติมในเบื้องหลัง (background)
         const promises = data.results.map(async (pokemon) => {
             const res = await fetch(pokemon.url)
             return res.json()
         })
-        //console.log(promises)
-        allPokemonData = await Promise.all(promises)
+
+        const pokemonData = await Promise.all(promises)
+        allPokemonData = pokemonData
         filteredPokemonData = allPokemonData
-        //console.log(typeof pokemonData)
-        // document.getElementById('prev-btn').disabled = page === 1
-        // document.getElementById('next-btn').disabled = pokemonData.length < limit
-        //console.log(page)
+
+        //console.log(filteredPokemonData)
     } catch (error) {
         console.log(error)
     } finally {
-        await new Promise((resolve) => setTimeout(resolve, 300))
         loader.classList.add('hidden')
         grid.classList.remove('hidden')
         renderPokemonPage()
@@ -50,22 +46,42 @@ async function fetchPokemon() {
 }
 
 async function changePage(direction) {
+    const loader = document.querySelector('.js-loader')
+    loader.classList.remove('hidden') // แสดง loader ก่อน
+    const grid = document.querySelector('.js-pokemon-grid')
+    grid.classList.add('hidden') // ซ่อน grid ขณะที่กำลังโหลด
+
     currentPage += direction
     renderPokemonPage()
-}
-function filterByType(type) {
-    filteredPokemonData = allPokemonData.filter((pokemon) => pokemon.types.some((t) => t.type.name === type))
 
-    currentPage = 1 // รีเซ็ตกลับไปหน้าแรกหลังจาก filter
-    renderPokemonPage()
+    await new Promise((resolve) => setTimeout(resolve, 200))
+    loader.classList.add('hidden')
+    grid.classList.remove('hidden')
 }
 
+//render
 function renderPokemonPage() {
-    const limit = 20
+    const limit = 12
     const startIndex = (currentPage - 1) * limit
     const endIndex = startIndex + limit
 
     const pokemonToDisplay = filteredPokemonData.slice(startIndex, endIndex)
+
+    if (pokemonToDisplay.length === 0) {
+        document.querySelector('.js-pokemon-notfound').innerHTML = `
+            <div class="text-center text-gray-500">
+                <h1 class="text-5xl">No Pokémon found. Try searching again.</h1>
+            </div>`
+        document.querySelector('.js-pokemon-grid').classList.add('hidden')
+        document.querySelector('.js-pokemon-notfound').classList.remove('hidden')
+
+        // ปิดปุ่ม Next/Previous ถ้าไม่มีผลลัพธ์
+        document.getElementById('prev-btn').disabled = true
+        document.getElementById('next-btn').disabled = true
+        return // จบการทำงานของฟังก์ชันนี้
+    }
+
+    document.querySelector('.js-pokemon-notfound').classList.add('hidden')
 
     renderPokemon(pokemonToDisplay)
 
@@ -76,7 +92,7 @@ function renderPokemonPage() {
 
 function renderPokemon(pokemonData) {
     let pokemonDisplay = ''
-
+    document.querySelector('.js-pokemon-grid').classList.remove('hidden')
     if (!Array.isArray(pokemonData)) {
         pokemonData = [pokemonData] // แปลง object เดี่ยวให้เป็นอาร์เรย์
         //console.log(typeof pokemonData)
@@ -93,18 +109,17 @@ function renderPokemon(pokemonData) {
         const firstBadgeClass = getTypeBadgeClass(firstType)
         const secondBadgeClass = secondType ? getTypeBadgeClass(secondType) : ''
 
-        //transition-all duration-150 ease-out hover:bg-gradient-to-r from-cyan-500 to-blue-500
         pokemonDisplay += `<div class="card bg-slate-700 w-full drop-shadow-lg transition ease-in-out delay-75 hover:drop-shadow-xl hover:-translate-y-6 cursor-pointer duration-300 js-card" data-pokemon-id="${index}"> 
                         <div class="flex items-center justify-center w-full h-[10rem]">
-                            <img width="100px" height="auto" src="${pokemon.sprites.other.dream_world.front_default}"/>
+                            <img width="80px" height="80px" src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}" loading="lazy"/>
                         </div>
                         <div class="card-body">
                             <h2 class="card-title text-2xl">
                                 ${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
                             </h2>
                             <div class="card-actions justify-end ">
-                                <div class="badge h-auto text-white text-lg border-none ${firstBadgeClass}">${firstType}</div>
-                                ${secondType ? `<div class="badge h-auto text-white text-lg border-none ${secondBadgeClass}">${secondType}</div>` : ''}
+                                <div class="badge h-auto text-white font-semibold text-lg bg-transparent border-2  ${firstBadgeClass}">${firstType}</div>
+                                ${secondType ? `<div class="badge h-auto text-white font-semibold text-lg bg-transparent border-2  ${secondBadgeClass}">${secondType}</div>` : ''}
                             </div>
                         </div>
             </div>`
@@ -122,7 +137,9 @@ function renderPokemon(pokemonData) {
         }
     })
 }
+//end render
 
+//search function
 const searchInput = document.querySelector('.js-search-input')
 const searchBtn = document.querySelector('.js-search-btn')
 
@@ -131,7 +148,8 @@ async function handleSearch() {
     if (pokemon) {
         await searchPokemon(pokemon)
     } else {
-        await fetchPokemon()
+        filteredPokemonData = allPokemonData
+        renderPokemonPage()
     }
 }
 
@@ -149,7 +167,8 @@ searchInput.addEventListener('keypress', (event) => {
 
 searchInput.addEventListener('input', async () => {
     if (searchInput.value.trim() === '') {
-        await fetchPokemon() // โหลด Pokémon ทั้งหมดอีกครั้งเมื่อ input ว่าง
+        filteredPokemonData = allPokemonData
+        renderPokemonPage()
     }
 })
 
@@ -162,53 +181,121 @@ async function searchPokemon(query) {
     try {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${query.toLowerCase()}`)
         if (!response.ok) throw new Error('Failed to fetch Pokémon list')
-        const data = await response.json()
-        pokemonData = data
+        const pokemon = await response.json()
+        filteredPokemonData = [pokemon]
     } catch (error) {
-        alert('Pokemon not found!')
+        filteredPokemonData = []
     } finally {
-        await new Promise((resolve) => setTimeout(resolve, 300))
         loader.classList.add('hidden')
         grid.classList.remove('hidden')
-        renderPokemon()
+        renderPokemonPage()
     }
 }
+//end search function
 
+//filter by type
+function filterByType(type) {
+    const loader = document.querySelector('.js-loader')
+    loader.classList.remove('hidden')
+    const grid = document.querySelector('.js-pokemon-grid')
+    grid.classList.add('hidden')
+
+    filteredPokemonData = allPokemonData.filter((pokemon) => pokemon.types.some((t) => t.type.name === type))
+
+    if (filteredPokemonData.length === 0) {
+        document.querySelector('.js-pokemon-notfound').innerHTML = `
+            <div class="text-center text-gray-500">
+                <h1 class="text-5xl">No Pokémon found. Try searching again.</h1>
+            </div>`
+        document.querySelector('.js-pokemon-grid').classList.add('hidden')
+        document.querySelector('.js-pokemon-notfound').classList.remove('hidden')
+
+        document.getElementById('prev-btn').disabled = true
+        document.getElementById('next-btn').disabled = true
+        setTimeout(() => {
+            loader.classList.add('hidden')
+            grid.classList.add('hidden')
+        }, 200)
+        return
+    }
+
+    currentPage = 1
+    renderPokemonPage()
+
+    setTimeout(() => {
+        loader.classList.add('hidden')
+        grid.classList.remove('hidden')
+    }, 200)
+}
 const types = ['normal', 'fire', 'water', 'grass', 'electric', 'ice', 'fighting', 'poison', 'ground', 'flying', 'psychic', 'bug', 'rock', 'ghost', 'dragon', 'dark', 'steel', 'fairy']
-const container = document.querySelector('.types-container');
+const container = document.querySelector('.types-container')
 
-types.forEach(type => {
-    const button = document.createElement('button');
-    button.classList.add('btn');
-    button.classList.add(type + '-btn'); // เพิ่มคลาสที่เป็นเอกลักษณ์ตามประเภท
+types.forEach((type) => {
+    const button = document.createElement('button')
+    button.classList.add('btn', 'btn-outline', 'btn-xs')
+    button.style.color = 'white'
+    button.style.borderColor = getProgressColor(type)
     //button.classList.add(`${getTypeBadgeClass(type)}`)
-    button.textContent = type.charAt(0).toUpperCase() + type.slice(1); // แสดงชื่อประเภท
-    container.appendChild(button); // เพิ่มปุ่มใน container
+    button.textContent = type.charAt(0).toUpperCase() + type.slice(1)
+    container.appendChild(button)
 
-    // เพิ่ม event listener ให้ปุ่มแต่ละตัว
+    button.addEventListener('mouseover', () => {
+        if (!button.classList.contains('btn-active')) {
+            button.style.color = '#ffffff'
+            button.style.backgroundColor = getProgressColor(type)
+        }
+    })
+    button.addEventListener('mouseout', () => {
+        if (!button.classList.contains('btn-active')) {
+            button.style.backgroundColor = 'transparent'
+        }
+    })
+
     button.addEventListener('click', () => {
-        filterByType(type); // เมื่อคลิกจะเรียกฟังก์ชัน filterByType พร้อมกับประเภทที่เลือก
-    });
-});
+        const isActive = button.classList.contains('btn-active')
 
+        //console.log('First State Click:', button.classList.contains('btn-active'))
+
+        document.querySelectorAll('.btn-outline').forEach((btn) => {
+            btn.classList.remove('btn-active')
+            btn.style.backgroundColor = 'transparent'
+        })
+
+        if (isActive) {
+            filteredPokemonData = allPokemonData
+            renderPokemonPage()
+            //console.log('Un select type click:', button.classList.contains('btn-active'))
+        } else {
+            button.style.backgroundColor = getProgressColor(type)
+            button.style.color = 'white'
+            //console.log('Select type click:', button.classList.contains('btn-active'))
+            button.classList.add('btn-active')
+            //console.log('After Select type click:', button.classList.contains('btn-active'))
+            filterByType(type)
+        }
+    })
+})
+//end filter by type
+
+//open detail pokemon with modal
 function openModal(index) {
-    const pokemon = allPokemonData[index]
+    const pokemon = filteredPokemonData[index]
     //console.log(pokemon)
     const firstType = pokemon.types[0]?.type.name || '-'
     const secondType = pokemon.types[1]?.type.name || null
     const firstBadgeClass = getTypeBadgeClass(firstType)
     const secondBadgeClass = secondType ? getTypeBadgeClass(secondType) : ''
 
-    document.getElementById('pokemon-modal').innerHTML = `<div class="modal-content bg-white rounded-lg mx-5 p-6 max-w-md lg:max-w-lg w-full h-auto relative">
-                        <button id="modal-close" class=" text-black top-4 right-4 absolute text-xl font-bold">❌</button>
+    document.getElementById('pokemon-modal').innerHTML = `<div class="modal-content bg-slate-700 rounded-lg mx-5 p-6 max-w-md lg:max-w-lg w-full h-auto relative">
+                        <button id="modal-close" class=" text-red-600 top-4 right-4 absolute text-xl font-bold">❌</button>
                         <div class="flex flex-col items-center justify-center">
-                            <img class="w-[150px] lg:w-[200px]" height="auto" src="${pokemon.sprites.other.dream_world.front_default}"/>
-                            <h2 id="modal-title" class="text-xl text-black font-bold mb-4">${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
+                            <img class="w-[100px] lg:w-[150px]" height="auto" src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}" loading="lazy"/>
+                            <h2 id="modal-title" class="text-xl text-white font-bold mb-4">${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h2>
                         </div>
                         <div class="card-actions justify-center ">
-                                <div class="badge h-auto text-white text-lg border-none ${firstBadgeClass}">${firstType}</div>
-                                ${secondType ? `<div class="badge h-auto text-white text-lg border-none ${secondBadgeClass}">${secondType}</div>` : ''}
-                        </div>
+                                <div class="badge h-auto text-white font-semibold text-lg bg-transparent border-2  ${firstBadgeClass}">${firstType}</div>
+                                ${secondType ? `<div class="badge h-auto text-white font-semibold text-lg bg-transparent border-2  ${secondBadgeClass}">${secondType}</div>` : ''}
+                            </div>
                         <div id="modal-body">
                         </div>
                     </div>`
@@ -217,7 +304,7 @@ function openModal(index) {
         .map(
             (stat) =>
                 `
-        <div class="flex justify-between flex-col lg:flex-row mt-4 items-center text-black">
+        <div class="flex justify-between flex-col lg:flex-row mt-4 items-center text-white">
             <span class="font-bold">${stat.stat.name.toUpperCase()} (${stat.base_stat}) </span>
             <progress id="myProgress" class="progress bg-slate-100 w-56" value="${stat.base_stat}" max="100" style="--progress-value-color: ${getProgressColor(firstType)};"></progress>
         </div>
@@ -230,8 +317,9 @@ function openModal(index) {
     document.getElementById('pokemon-modal').classList.remove('hidden')
 
     document.getElementById('modal-close').addEventListener('click', closeModal)
+    document.getElementById('pokemon-modal').addEventListener('click', closeModal)
 }
-
+//close modal
 function closeModal() {
     const modal = document.getElementById('pokemon-modal')
     const modalContent = document.querySelector('.modal-content')
@@ -245,24 +333,24 @@ function closeModal() {
 
 function getTypeBadgeClass(type) {
     const typeColors = {
-        normal: 'bg-gray-400',
-        fire: 'bg-red-500',
-        water: 'bg-blue-500',
-        grass: 'bg-green-500',
-        electric: 'bg-yellow-400',
-        ice: 'bg-cyan-400',
-        fighting: 'bg-red-700',
-        poison: 'bg-purple-500',
-        ground: 'bg-yellow-600',
-        flying: 'bg-blue-300',
-        psychic: 'bg-pink-500',
-        bug: 'bg-green-700',
-        rock: 'bg-gray-600',
-        ghost: 'bg-indigo-500',
-        dragon: 'bg-purple-700',
-        dark: 'bg-gray-800',
-        steel: 'bg-gray-300',
-        fairy: 'bg-pink-300',
+        normal: 'border-gray-400',
+        fire: 'border-red-500',
+        water: 'border-blue-500',
+        grass: 'border-green-500',
+        electric: 'border-yellow-400',
+        ice: 'border-cyan-400',
+        fighting: 'border-red-700',
+        poison: 'border-purple-500',
+        ground: 'border-yellow-600',
+        flying: 'border-blue-300',
+        psychic: 'border-pink-500',
+        bug: 'border-lime-400',
+        rock: 'border-gray-600',
+        ghost: 'border-indigo-500',
+        dragon: 'border-purple-700',
+        dark: 'border-gray-800',
+        steel: 'border-gray-300',
+        fairy: 'border-pink-300',
     }
     return typeColors[type] || 'bg-gray-200' // สีเริ่มต้นหากไม่เจอ type
 }
@@ -274,13 +362,13 @@ function getProgressColor(type) {
         water: '#6390f0',
         grass: '#22c55e',
         electric: '#f7d02c',
-        ice: '##22d3ee',
+        ice: '#22d3ee',
         fighting: '#c22e28',
         poison: '#a33ea1',
-        ground: '##ca8a04',
+        ground: '#ca8a04',
         flying: '#93c5fd',
         psychic: '#ec4899',
-        bug: '#15803d',
+        bug: '#a3e635',
         rock: '#4b5563',
         ghost: '#6366f1',
         dragon: '#7e22ce',
